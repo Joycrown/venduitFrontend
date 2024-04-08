7681e2870504eef3c386f29a0fc50fb9597120d9
Joycrown <olaniyanayoade999@gmail.com>
2024-04-08 15:37:13 +0100

    landing page

--- src/containers/landingPage/LandingPage.tsx
+++ src/containers/landingPage/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Link, VStack } from "@chakra-ui/react";
 import LandingHeader from "../../components/LandingComponents/landingHeader";
 import UnderConstruction from "../extras/UnderConstruction";
 
@@ -7,6 +7,41 @@ const LandingPage = () => {
     <Container as="section" maxW="8xl" p={5} minH="100vh">
       <LandingHeader />
       <UnderConstruction/>
+      <VStack>
+        <Link
+          href="/login"
+          variant="outline"
+          color="white"
+          display="flex"
+          justifyContent="center"
+          bgColor="blue"
+          p={3}
+          mt={5}
+          width={{base:"100%",md:"95%",lg:"50%"}}
+          size="sm"
+          border="1px"
+          borderRadius={50}
+        >
+          Log in
+        </Link>
+        <Link
+          href="/signup"
+          variant="outline"
+          color="white"
+          display="flex"
+          justifyContent="center"
+          bgColor="blue"
+          p={3}
+          mt={5}
+          width={{base:"100%",md:"95%",lg:"50%"}}
+          size="sm"
+          border="1px"
+          borderRadius={50}
+        >
+          Sign Up
+        </Link>
+
+      </VStack>
     </Container>
   );
 };
